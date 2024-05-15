3cb2edfb24b6956c91bb3bb3a9ceb44c5a92bf83
marcel325 <165882495+marcel325@users.noreply.github.com>
2024-05-15 12:09:54 +0700

    Add files via upload

---
+++ deleteData.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "database";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if (isset($_GET['id'])) {
+    $id = intval($_GET['id']);
+    $sql = "DELETE FROM complaints WHERE id = $id";
+
+    if ($conn->query($sql) === TRUE) {
+        echo json_encode(["success" => true]);
+    } else {
+        echo json_encode(["success" => false]);
+    }
+} else {
+    echo json_encode(["success" => false]);
+}
+
+$conn->close();
+?>
---
+++ getData.php
@@ -0,0 +1,28 @@
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "database";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT id, name, email, complaint FROM complaints";
+$result = $conn->query($sql);
+
+$data = array();
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+} 
+
+echo json_encode($data);
+
+$conn->close();
+?>
